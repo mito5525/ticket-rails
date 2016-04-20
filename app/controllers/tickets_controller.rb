88b17fe4d73ceaaa66d6941ab6a3d3c4daad8dc4
Tomohiko Mimura <mito.5525@gmail.com>
2016-04-20 23:09:20 +0900

    rubocop --auto-correct

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -62,13 +62,14 @@ class TicketsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_ticket
-      @ticket = Ticket.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def ticket_params
-      params.require(:ticket).permit(:title, :descripution, :priority, :status)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_ticket
+    @ticket = Ticket.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def ticket_params
+    params.require(:ticket).permit(:title, :descripution, :priority, :status)
+  end
 end
