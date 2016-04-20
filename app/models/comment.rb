686159469d1f1d7becedb8432b15a1de2173cca4
Tomohiko Mimura <mito.5525@gmail.com>
2016-04-20 19:40:21 +0900

    Gem: annotateを追加する
    $ bin/rails generate annotate:install
    
    - モデルファイルにスキーマコメントを付加する

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :integer          not null, primary key
+#  body       :text(65535)
+#  ticket_id  :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  fk_rails_e013b60ecb  (ticket_id)
+#
+
 class Comment < ActiveRecord::Base
   belongs_to :ticket
 end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: tickets
+#
+#  id           :integer          not null, primary key
+#  title        :string(255)
+#  descripution :text(65535)
+#  priority     :integer
+#  status       :integer
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 class Ticket < ActiveRecord::Base
   enum priority: { hight: 0, mid: 1, low: 3 }
   enum status: { not_comp: 0, wip: 1, comp: 3 }
