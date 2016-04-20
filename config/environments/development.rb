b66d1cec6aa410f629e75d300263f6a39f6182eb
Tomohiko Mimura <mito.5525@gmail.com>
2016-04-20 22:27:41 +0900

    BetterErrors と WebConsoleをVagrant(VM)に対応させる
    $ export TRUSTED_IP=11.11.11.1

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,9 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  if ENV['TRUSTED_IP']
+    BetterErrors::Middleware.allow_ip! ENV['TRUSTED_IP']
+    config.web_console.whitelisted_ips = ENV['TRUSTED_IP']
+  end
 end
