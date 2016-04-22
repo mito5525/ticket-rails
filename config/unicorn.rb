b0a243a4134b2520521b35132645e3cb15c55f83
Tomohiko Mimura <mito.5525@gmail.com>
2016-04-22 23:39:38 +0900

    unicornの設定を修正する
    - Vagrant共有フォルダ内にはunicorn.socketは作成できないため、ローカルで試す場合は注意

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -8,6 +8,10 @@
 # See http://unicorn.bogomips.org/Unicorn/Configurator.html for complete
 # documentation.
 
+rails_root = File.expand_path('../../', __FILE__)
+
+ENV['BUNDLE_GEMFILE'] = rails_root + '/Gemfile'
+
 # Use at least one worker per core if you're on a dedicated server,
 # more will usually help for _short_ waits on databases/caches.
 worker_processes 4
@@ -21,24 +25,24 @@ worker_processes 4
 
 # Help ensure your application will always spawn in the symlinked
 # "current" directory that Capistrano sets up.
-working_directory "/path/to/app/current" # available in 0.94.0+
+working_directory rails_root # available in 0.94.0+
 
 # listen on both a Unix domain socket and a TCP port,
 # we use a shorter backlog for quicker failover when busy
-listen "/path/to/.unicorn.sock", :backlog => 64
-listen 8080, :tcp_nopush => true
+listen File.expand_path('tmp/sockets/unicorn.sock', rails_root), backlog: 64
+listen 8080, tcp_nopush: true
 
 # nuke workers after 30 seconds instead of 60 seconds (the default)
 timeout 30
 
 # feel free to point this anywhere accessible on the filesystem
-pid "/path/to/app/shared/pids/unicorn.pid"
+pid File.expand_path('tmp/pids/unicorn.pid', rails_root)
 
 # By default, the Unicorn logger will write to stderr.
 # Additionally, ome applications/frameworks log to stderr or stdout,
 # so prevent them from going to /dev/null when daemonized here:
-stderr_path "/path/to/app/shared/log/unicorn.stderr.log"
-stdout_path "/path/to/app/shared/log/unicorn.stdout.log"
+stderr_path File.expand_path('log/unicorn.stderr.log', rails_root)
+stdout_path File.expand_path('log/unicorn.stdout.log', rails_root)
 
 # combine Ruby 2.0.0+ with "preload_app true" for memory savings
 preload_app true
@@ -71,21 +75,21 @@ before_fork do |server, worker|
   # The following is only recommended for memory/DB-constrained
   # installations.  It is not needed if your system can house
   # twice as many worker_processes as you have configured.
-  #
-  # # This allows a new master process to incrementally
-  # # phase out the old master process with SIGTTOU to avoid a
-  # # thundering herd (especially in the "preload_app false" case)
-  # # when doing a transparent upgrade.  The last worker spawned
-  # # will then kill off the old master process with a SIGQUIT.
-  # old_pid = "#{server.config[:pid]}.oldbin"
-  # if old_pid != server.pid
-  #   begin
-  #     sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
-  #     Process.kill(sig, File.read(old_pid).to_i)
-  #   rescue Errno::ENOENT, Errno::ESRCH
-  #   end
-  # end
-  #
+
+  # This allows a new master process to incrementally
+  # phase out the old master process with SIGTTOU to avoid a
+  # thundering herd (especially in the "preload_app false" case)
+  # when doing a transparent upgrade.  The last worker spawned
+  # will then kill off the old master process with a SIGQUIT.
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if old_pid != server.pid
+    begin
+      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+      Process.kill(sig, File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+    end
+  end
+
   # Throttle the master from forking too quickly by sleeping.  Due
   # to the implementation of standard Unix signal handlers, this
   # helps (but does not completely) prevent identical, repeated signals
@@ -108,4 +112,3 @@ after_fork do |server, worker|
   # between any number of forked children (assuming your kernel
   # correctly implements pread()/pwrite() system calls)
 end
-
